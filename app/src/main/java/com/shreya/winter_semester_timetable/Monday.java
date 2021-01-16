fa951ebf0ec51d84ea829abd2731dcd21d3d673c
Shreya <shreyadalal34@gmail.com>
2021-01-16 23:32:05 +0530

    This commit contains the logic of 2nd activity

--- app/src/main/java/com/shreya/winter_semester_timetable/Monday.java
+++ app/src/main/java/com/shreya/winter_semester_timetable/Monday.java
@@ -2,13 +2,27 @@ package com.shreya.winter_semester_timetable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Monday extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monday);
+
+        Intent intent = getIntent();
+        String day = intent.getStringExtra(MainActivity.MSG);
+        TextView textView = findViewById(R.id.textView2);
+        textView.setText(day);
+
+        textView = findViewById(R.id.textView10);
+        textView.setText("Computer\n Networks");
+
+        textView = findViewById(R.id.textView12);
+        textView.setText("System\n Programming");
     }
 }
