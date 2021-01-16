2d2c9be5a38c80bc7f7a62e2232bcdfae9ee24d8
Shreya <shreyadalal34@gmail.com>
2021-01-16 23:41:13 +0530

    Commit contains logic of 3rd Activity

--- app/src/main/java/com/shreya/winter_semester_timetable/Tuesday.java
+++ app/src/main/java/com/shreya/winter_semester_timetable/Tuesday.java
@@ -2,7 +2,9 @@ package com.shreya.winter_semester_timetable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Tuesday extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class Tuesday extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tuesday);
+
+        Intent intent = getIntent();
+        String day = intent.getStringExtra(MainActivity.MSG);
+        TextView textView = findViewById(R.id.textView2);
+        textView.setText(day);
+
+        textView = findViewById(R.id.textView6);
+        textView.setText("Object\n Oriented\n Programming");
+
+        textView = findViewById(R.id.textView12);
+        textView.setText("Web\n Programming");
     }
 }
